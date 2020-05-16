3edfbf24e46c428dcb27661a2d6fbf33df5119b8
adityadesaia3 <adityadesaia3@gmail.com>
2020-05-17 01:30:09 +0530

    1. added 'Add Birthdate functionality' 2. displayed birthdates 3. Update birthdate functionality 4. Delete birthdate functionality

--- birthdate_db.py
+++ birthdate_db.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-def add_birthdate_to_db(name, birthdate):
+def get_max_id():
     connection = None
     try:
         connection = mysql.connector.connect(host="localhost", user="root", password="", database="birthdate_keeper_db")
@@ -9,7 +9,88 @@ def add_birthdate_to_db(name, birthdate):
 
             try:
                 # insert data
-                insert_query = f"insert into birthdate_keeper_tb(name, birthdate) values ('{name}', '{birthdate}')"
+                select_query = f"select max(id) from birthdate_keeper_tb"
+                cursor.execute(select_query)
+                max_id = cursor.fetchone()[0]
+                
+            except mysql.connector.Error as error:
+                print(error)
+            finally:
+                cursor.close()
+    except mysql.connector.Error as error:
+        print(error)
+    finally:
+        if connection:
+            connection.commit()
+            connection.close()
+    if max_id:
+        return max_id
+    else:
+        return 0
+
+
+def fetch_birthdates_from_db():
+    birthdate_data = None
+    connection = None
+    try:
+        connection = mysql.connector.connect(host="localhost", user="root", password="", database="birthdate_keeper_db")
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            try:
+                # insert data
+                select_query = f"select * from birthdate_keeper_tb"
+                cursor.execute(select_query)
+                birthdate_data = cursor.fetchall()
+            except mysql.connector.Error as error:
+                print(error)
+            finally:
+                cursor.close()
+    except mysql.connector.Error as error:
+        print(error)
+    finally:
+        if connection:
+            connection.commit()
+            connection.close()
+    return birthdate_data
+
+
+def fetch_single_birthdate_from_db(b_id):
+    birthdate_data = None
+    connection = None
+    try:
+        connection = mysql.connector.connect(host="localhost", user="root", password="", database="birthdate_keeper_db")
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            try:
+                # insert data
+                select_query = f"select * from birthdate_keeper_tb where id = {b_id}"
+                cursor.execute(select_query)
+                birthdate_data = cursor.fetchall()[0]
+            except mysql.connector.Error as error:
+                print(error)
+            finally:
+                cursor.close()
+    except mysql.connector.Error as error:
+        print(error)
+    finally:
+        if connection:
+            connection.commit()
+            connection.close()
+    return birthdate_data
+
+
+def add_birthdate_to_db(name, birthdate, photo_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(host="localhost", user="root", password="", database="birthdate_keeper_db")
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            try:
+                # insert data
+                insert_query = f"insert into birthdate_keeper_tb(name, birthdate, photo_name) values ('{name}', '{birthdate}', '{photo_name}')"
                 cursor.execute(insert_query)
             except mysql.connector.Error as error:
                 print(error)
@@ -21,6 +102,53 @@ def add_birthdate_to_db(name, birthdate):
         if connection:
             connection.commit()
             connection.close()
+
+
+def update_birthdate_from_db(b_id, name, birthdate, photo_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(host="localhost", user="root", password="", database="birthdate_keeper_db")
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            try:
+                # update data
+                update_query = f"update birthdate_keeper_tb set name = '{name}', birthdate = '{birthdate}', photo_name = '{photo_name}' where id = {b_id}"
+                cursor.execute(update_query)
+            except mysql.connector.Error as error:
+                print(error)
+            finally:
+                cursor.close()
+    except mysql.connector.Error as error:
+        print(error)
+    finally:
+        if connection:
+            connection.commit()
+            connection.close()
+
+
+def delete_birthdate_from_db(b_id):
+    connection = None
+    try:
+        connection = mysql.connector.connect(host="localhost", user="root", password="", database="birthdate_keeper_db")
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            try:
+                # delete data
+                delete_query = f"delete from birthdate_keeper_tb where id = {b_id}"
+                cursor.execute(delete_query)
+            except mysql.connector.Error as error:
+                print(error)
+            finally:
+                cursor.close()
+    except mysql.connector.Error as error:
+        print(error)
+    finally:
+        if connection:
+            connection.commit()
+            connection.close()
+
 
 def create_db():
     connection = None
@@ -40,7 +168,7 @@ def create_db():
                 cursor = connection.cursor()
                 
                 # Adding table to newly created database
-                cursor.execute("CREATE TABLE birthdate_keeper_tb (id INT NOT NULL AUTO_INCREMENT , name TEXT NOT NULL , birthdate TINYTEXT NOT NULL, image BLOB , PRIMARY KEY (id))")
+                cursor.execute("CREATE TABLE birthdate_keeper_tb (id INT NOT NULL AUTO_INCREMENT , name TEXT NOT NULL , birthdate TINYTEXT NOT NULL, photo_name TEXT , PRIMARY KEY (id))")
             except mysql.connector.Error as error:
                 print(error)
             finally:
